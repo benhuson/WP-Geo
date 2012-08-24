4e2354a1fd0632b22b033dbee4d3dfbe5b29f7b5
Ben Huson <ben@thewhiteroom.net>
2012-08-24 22:26:19 +0100

    Added forum and bug links.

--- admin/donate-links.php
+++ admin/donate-links.php
@@ -7,8 +7,9 @@
 ?>
 
 <ul class="subsubsub donate-links" style="float:none;">
-	<li><?php echo __( 'Support this plugin', 'wp-geo' ); ?>: </li>
-	<li><a href="http://www.wpgeo.com/donate" target="_blank"><?php echo __( 'Make a donation', 'wp-geo' ); ?></a> | </li>
-	<li><a href="http://twitter.com/wpgeo" target="_blank"><?php echo __( 'Follow @wpgeo for updates', 'wp-geo' ); ?></a> | </li>
+	<li><a href="http://wordpress.org/support/plugin/wp-geo" target="_blank"><?php echo __( 'Support Forum', 'wp-geo' ); ?></a> &nbsp; | &nbsp; </li>
+	<li><a href="https://github.com/benhuson/WP-Geo/issues" target="_blank"><?php echo __( 'Report Bugs', 'wp-geo' ); ?></a> &nbsp; | &nbsp; </li>
+	<li><a href="http://www.wpgeo.com/donate" target="_blank"><?php echo __( 'Make a donation', 'wp-geo' ); ?></a> &nbsp; | &nbsp; </li>
+	<li><a href="http://twitter.com/wpgeo" target="_blank"><?php echo __( 'Follow @wpgeo for updates', 'wp-geo' ); ?></a> &nbsp; | &nbsp; </li>
 	<li><a href="http://twitter.com/benhuson" target="_blank"><?php echo __( 'Follow me on Twitter', 'wp-geo' ); ?></a></li>
 </ul>
