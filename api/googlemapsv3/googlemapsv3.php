149cb30bc5d27090c0c841b09b418048472c11f6
Ben Huson <ben@thewhiteroom.net>
2014-04-24 23:36:28 +0100

    Google Maps API v3 URL does not require API key.

--- api/googlemapsv3/googlemapsv3.php
+++ api/googlemapsv3/googlemapsv3.php
@@ -26,14 +26,27 @@ class WPGeo_API_GoogleMapsV3 extends WPGeo_API {
 	 */
 	function wpgeo_register_scripts() {
 		global $wpgeo;
-		$googlemaps_js = add_query_arg( array(
-			'language' => $wpgeo->get_googlemaps_locale(),
-			'key'      => $wpgeo->get_google_api_key(),
-			'sensor'   => 'false'
-		), '//maps.googleapis.com/maps/api/js' );
-		wp_register_script( 'googlemaps3', $googlemaps_js, false, $wpgeo->version );
+		wp_register_script( 'googlemaps3', $this->get_googlemaps3_script_url(), false, $wpgeo->version );
 		wp_register_script( 'wpgeo', WPGEO_URL . 'js/wp-geo.v3.js', array( 'jquery', 'wpgeo_tooltip' ), $wpgeo->version );
 		wp_register_script( 'wpgeo_admin_post_googlemaps3', WPGEO_URL . 'api/googlemapsv3/js/admin-post.js', array( 'jquery', 'wpgeo_admin_post', 'googlemaps3' ), $wpgeo->version );
+	}
+
+	/**
+	 * Get Google Maps v3 Script URL
+	 *
+	 * @return  string  Google Maps API v3 URL.
+	 */
+	function get_googlemaps3_script_url() {
+		global $wpgeo;
+		$googlemaps_js_args = array(
+			'language' => $wpgeo->get_googlemaps_locale(),
+			'sensor'   => 'false'
+		);
+		$api_key = $wpgeo->get_google_api_key();
+		if ( ! empty( $api_key ) ) {
+			$googlemaps_js_args['key'] = $api_key;
+		}
+		return add_query_arg( $googlemaps_js_args, '//maps.googleapis.com/maps/api/js' );
 	}
 
 	/**
