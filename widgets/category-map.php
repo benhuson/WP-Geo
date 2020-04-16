b54c89beb95ddffac9b48865a70cfc3ab1f5f3a8
flynsarmy <flynsarmy@gmail.com>
2020-04-16 10:14:24 +1000

    create_function() is deprecated

--- widgets/category-map.php
+++ widgets/category-map.php
@@ -97,4 +97,6 @@ class WPGeo_Category_Map_Widget extends WPGeo_Widget {
 }
 
 // Widget Hook
-add_action( 'widgets_init', create_function( '', 'return register_widget( "WPGeo_Category_Map_Widget" );' ) );
+add_action( 'widgets_init', function() {
+	return register_widget( "WPGeo_Category_Map_Widget" );
+});
